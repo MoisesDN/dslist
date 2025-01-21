eb02ed6557775ed0a58fd3c4f51fe20de19ada71
MoisesDN <m.dias009@gmail.com>
2025-01-21 10:50:50 -0300

    GameDTO, busca game por id

--- src/main/java/com/moisesdias/dslist/services/GameService.java
+++ src/main/java/com/moisesdias/dslist/services/GameService.java
@@ -1,9 +1,12 @@
 package com.moisesdias.dslist.services;
 
+import com.moisesdias.dslist.dto.GameDTO;
 import com.moisesdias.dslist.dto.GameMinDTO;
+import com.moisesdias.dslist.entities.Game;
 import com.moisesdias.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +16,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         var result = gameRepository.findAll();
         //return result.stream().map(x -> new GameMinDTO(x)).toList();
